aaf317aaf31f16295e14ffaaa7478ec879199192
Aneesh-Tayal <aneesh0034.becse24@chitkara.edu.in>
2025-04-15 22:54:28 +0530

    Addition of OAuth Login

---
+++ backend/OAuth/appwrite.js
@@ -0,0 +1,28 @@
+import {Client, Databases, Account} from 'node-appwrite';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const createAppwriteClient = async (type, session) => {
+    const {ENDPOINT, PROJECT_ID, API_KEY} = process.env;
+    const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+
+    if (type == "admin"){
+        client.setKey(API_KEY)
+    }
+
+    if (type=="session" && session){
+        client.setSession(session)
+    }
+
+    return {
+        get account (){
+            return new Account(client)
+        },
+        get databases(){
+            return new Databases(client)
+        }
+    }
+};
+
+export default createAppwriteClient;
